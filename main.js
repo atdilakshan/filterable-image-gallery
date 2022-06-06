d03dd6e95b92dea0674507be75f9f527ae24383b
atdilakshan <dilakshan0501@gmail.com>
2022-06-06 12:23:58 +0530

    modified all files

--- main.js
+++ main.js
@@ -1,23 +1,23 @@
+let catList = document.querySelectorAll(".cat-list");
+let box = document.querySelectorAll(".box");
 
- const filterContainer = document.querySelector(".gallery-filter"),
- galleryItems = document.querySelectorAll(".gallery-item");
+for(let i = 0; i<catList.length; i++){
+    catList[i].addEventListener('click', function(){
+        for(let j = 0; j<catList.length; j++){
+            catList[j].classList.remove("active");
+        }
+        this.classList.add("active")
 
- filterContainer.addEventListener("click", (event) =>{
-   if(event.target.classList.contains("filter-item")){
-   	 // deactivate existing active 'filter-item'
-   	 filterContainer.querySelector(".active").classList.remove("active");
-   	 // activate new 'filter-item'
-   	 event.target.classList.add("active");
-   	 const filterValue = event.target.getAttribute("data-filter");
-   	 galleryItems.forEach((item) =>{
-       if(item.classList.contains(filterValue) || filterValue === 'all'){
-       	item.classList.remove("hide");
-       	 item.classList.add("show");
-       }
-       else{
-       	item.classList.remove("show");
-       	item.classList.add("hide");
-       }
-   	 });
-   }
- });
+        let dataFilter = this.getAttribute("data-filter");
+
+        for(let k = 0; k<box.length; k++){
+            box[k].classList.remove("active");
+            box[k].classList.add("hide");
+
+            if(box[k].getAttribute("data-item") == dataFilter || dataFilter == "all"){
+                box[k].classList.remove("hide");
+                box[k].classList.add("active");
+            }
+        }
+    });
+}
